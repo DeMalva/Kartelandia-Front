12a78cb547cd43a4747ed97183bc5efb90df056a
Demalva <Demalva74@gmail.com>
2024-12-05 14:51:16 +0100

    Add server URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,31 +1,8 @@
-// export const ENV = {
-//     SERVER_HOST: "https://ecommerce-strapi.up.railway.app",
-//     API_URL: "https://ecommerce-strapi.up.railway.app/api",
-//     ENDPOINTS: {
-//       AUTH: {
-//         REGISTER: "auth/local/register",
-//         LOGIN: "auth/local",
-//       },
-//       USERS_ME: "users/me",
-//       USERS: "users",
-//       PLATFORM: "platforms",
-//       ADDRESS: "addresses",
-//       GAME: "games",
-//       WISHLIST: "wishlists",
-//       PAYMENY_ORDER: "payment-order",
-//       ORDER: "orders",
-//     },
-//     TOKEN: "token",
-//     CART: "cart",
-//     STRIPE_TOKEN:
-//       "pk_test_51McFNvGuOrnPfQra2CPEv40T6bwjvke3EmtZELp3685TKzCMDgagCauuO8nOVgL2sI4eiU0s830K3aZj31iOXtoV00zA9gPqMd",
-//   };
-
 export const ENV = {
-  // SERVER_HOST: "//localhost:1337",
-  // API_URL: "//localhost:1337/api",
-  SERVER_HOST: "http://127.0.0.1:1337",
-  API_URL: "http://127.0.0.1:1337/api",
+  SERVER_HOST: "http://kartelandia.up.railway.app",
+  API_URL: "http://kartelandia.up.railway.app/api",
+  // SERVER_HOST: "http://127.0.0.1:1337",
+  // API_URL: "http://127.0.0.1:1337/api",
   ENDPOINTS: {
     AUTH: {
       REGISTER: "auth/local/register",
